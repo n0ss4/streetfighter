6dafc101626b9ef80744b83e6d42a388258851eb
Nossair <n0ss4@protonmail.com>
2019-03-12 16:09:17 +0100

    Bootstrap gente

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>Aqui manda el Nossa</h1>
+    <p>Ayachi es mi enemigo</p>
+  </div>
+</template>
+
+<script>
+alert("Hola");
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <h1>STREET FIGHTER</h1>
+    <input type="text" placeholder="Introdueix el teu nom: ">
+    <button>Començar</button>
+  </div>
+</template>
