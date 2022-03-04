18674bc46ae22ab76b9118d28e05827ec5e5438c
n0ss4 <nghazouani@outlook.com>
2022-03-04 12:00:29 +0100

    Creació de diferents vistes del projecte

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Ganaste from './views/Ganar.vue'
+import Perdiste from './views/Perder.vue'
 
 Vue.use(Router)
 
@@ -14,10 +17,17 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: '/ganaste',
+      name: 'ganaste',
+      component: Ganaste
+    },
+    {
+      path: '/perdiste',
+      name: 'perdiste',
+      component: Perdiste
     }
   ]
 })
---
+++ src/views/Ganar.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="ganar">
+        <div class="w-100 justify-content-center d-flex">
+                <iframe class="justify-content-center" width="50%" src="https://giphy.com/embed/2Fazg78fL1pycJnnW" frameborder="0"></iframe>
+        </div>
+        <p><a href="https://giphy.com/gifs/mlb-2Fazg78fL1pycJnnW">via GIPHY</a></p>
+    </div>
+</template>
